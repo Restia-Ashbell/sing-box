42f3a949959700aaa449127b3f74c55c198f56d4
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2024-10-22 16:48:22 +0800

    fix: CacheFile is not required for concurrent URL Test

--- box.go
+++ box.go
@@ -61,7 +61,7 @@ func New(options Options) (*Box, error) {
 	var needCacheFile bool
 	var needClashAPI bool
 	var needV2RayAPI bool
-	if experimentalOptions.CacheFile != nil && experimentalOptions.CacheFile.Enabled || options.PlatformLogWriter != nil {
+	if experimentalOptions.CacheFile != nil && experimentalOptions.CacheFile.Enabled {
 		needCacheFile = true
 	}
 	if experimentalOptions.ClashAPI != nil || options.PlatformLogWriter != nil {
